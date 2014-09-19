2626440de791e1887621e6d67a9ab7f33d7f06ac
franz151 <trevor@saberemail.com>
2014-09-18 19:34:30 -0700

    First Upload

---
+++ VariablePlayground.playground/section-1.swift
@@ -0,0 +1,16 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var firstInteger:Int = 0 , secondInteger:Int = 29
+
+var thirdInteger:Float = -1000858
+
+var x:Int = 5
+
+var discountAtDepartmentStore:Float = 0.3
+
+// Here is a comment
+
+/* Here is another a comment
+which is a very useful tool*/
